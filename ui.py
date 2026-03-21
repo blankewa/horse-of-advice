25a63d8ad241e785485ead9a38d7010b75ab4e30
blankewa <ewan.mcmahon2@gmail.com>
2026-03-21 21:46:18 +0000

    add basic ui functions + horse image

--- ui.py
+++ ui.py
@@ -1,5 +1,22 @@
 #ui functions
 
-#import tkinter libs
-import tkinter as ui
+#import tkinter libs + pil
+import tkinter
+from PIL import Image
+
+#create windwo parent
+window = tkinter.Tk()
+
+#load image to tk form
+def load_image(path):
+    image = Image.open(path)
+    return tkinter.PhotoImage(image)
+
+#create main window in func
+def create_window():
+    #set basics of window
+    window.title("Wise Horse of Financial Wisdom")
+    window.geometry("400x400")
+
+
 
